cf8692027d9383bcc437740527efc140fac5298e
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-25 22:50:14 +0530

    add basic layout of budget tracker

---
+++ src/components/Budget.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Budget = () => {
+    return (
+        <div className="alert alert-secondry">
+            <span>Budget: Rs2000</span>
+            
+        </div>
+    );
+};
+
+export default Budget;
---
+++ src/components/Remaining.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Remaining = () => {
+    return (
+        <div className="alert alert-success">
+            <span>Remaining: Rs1000</span>
+            
+        </div>
+    );
+};
+
+export default Remaining;
