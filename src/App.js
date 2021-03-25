a1697769b25b908917080f198c928f9297797fa3
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-26 00:07:10 +0530

    add expense components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Budget from "./components/Budget";
 import Remaining from "./components/Remaining";
-import ExpenseTotal from "./components/ExpenseTotal";
-import ExpenseList from "./components/ExpenseList";
+import ExpenseTotal from "./components/Expense/ExpenseTotal";
+import ExpenseList from "./components/Expense/ExpenseList";
+import AddExpenseForm from "./components/Expense/AddExpenseForm";
 
 function App() {
   return (
@@ -19,11 +20,17 @@ function App() {
         <div className="col-sm">
           <ExpenseTotal />
         </div>
-        <h3 className="mt-3">Expenses</h3>
-        <div className="row-mt-3">
-          <div className="col-sm">
-            <ExpenseList />
-          </div>
+      </div>
+      <h3 className="mt-3">Expenses</h3>
+      <div className="row-mt-3">
+        <div className="col-sm">
+          <ExpenseList />
+        </div>
+      </div>
+      <h3 className='mt-3'>Add Expenses</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <AddExpenseForm />
         </div>
       </div>
     </div>
